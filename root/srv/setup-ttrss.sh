0909e6898078031650bd3051ccc8a469fd5e157b
papaschloss <11808509+papaschloss@users.noreply.github.com>
2018-11-21 11:50:27 -0500

    Update setup-ttrss.sh

--- root/srv/setup-ttrss.sh
+++ root/srv/setup-ttrss.sh
@@ -67,7 +67,7 @@ setup_ttrss()
         git clone --depth=1 https://github.com/papaschloss/newspaper-fulltext.git ${TTRSS_PATH}/plugins/newspaper_fulltext
         git clone --depth=1 https://github.com/m42e/ttrss_plugin-feediron.git ${TTRSS_PATH}/plugins/feediron
         git clone --depth=1 https://github.com/levito/tt-rss-feedly-theme.git ${TTRSS_PATH}/themes/feedly-git
-        composer require andreskrey/readability.php
+       # composer require andreskrey/readability.php
     fi
 
     # Add initial config.
